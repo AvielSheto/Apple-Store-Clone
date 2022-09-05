48596989ecac6aa7133f74421b7b5c47fd3526fa
aviel sheto <avielsheto@gmail.com>
2022-09-05 10:42:43 +0300

    update project

--- src/Components/EditProduct.jsx
+++ src/Components/EditProduct.jsx
@@ -10,17 +10,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
+import DeleteIcon from '@mui/icons-material/Delete';
+import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
 
 export default function EditProduct() {
     const dispatch = useDispatch();
     const { id } = useParams();
     const storeData = useSelector(state => state);
-    const [item, setItem] = useState({ id: id, name: "", price: ""})
+    const [item, setItem] = useState({ id: id, name: "", price: "" })
 
-    useEffect(()=>{
-       {storeData.products.filter(products => products.id === id).map((item)=>{
-        return(setItem({id: id, name:`${item.name}`, price:`${item.price}`, quantity: 1, img:`${item.img}`}))
-       })}
+    useEffect(() => {
+        {
+            storeData.products.filter(products => products.id === id).map((item) => {
+                return (setItem({ id: id, name: `${item.name}`, price: `${item.price}`, quantity: 1, img: `${item.img}` }))
+            })
+        }
     },[])
 
     const updateProduct = () => {
@@ -39,27 +43,26 @@ export default function EditProduct() {
 
     return (
         <Container >
-            <div className="product" style={{ margin: "2rem", padding: "2rem", border: "2px solid black", borderRadius:"15px"}}>
+            <div className="product" style={{ margin: "2rem", padding: "2rem", border: "2px solid black", borderRadius: "15px" }}>
                 <h1 style={{ textAlign: "center" }}>Edit Product</h1>
                 <h4>Updating product ID: {id} </h4>
                 <br />
-                <div style={{display:'flex', flexDirection:"column"}}>
+                <div style={{ display: 'flex', flexDirection: "column" }}>
                     <TextField onChange={handleChange} name='name' label="Name:" variant="outlined" />
                     <br />
                     <TextField onChange={handleChange} name='price' label="Price:" variant="outlined" />
                 </div>
-                
                 <br />
-                <div style={{ display: 'flex', justifyContent: "space-evenly", padding:"0px 30%"}}>
-                    <Button onClick={updateProduct} variant="contained">Update</Button>
-                    <Button onClick={deleteProduct} variant="contained">Delete</Button>
+                <div style={{ display: 'flex', justifyContent: "space-evenly", padding: "0px 30%" }}>
+                    <Button onClick={updateProduct} variant="contained" endIcon={<DriveFileRenameOutlineIcon/>}>Update</Button>
+                    <Button onClick={deleteProduct} variant="outlined" startIcon={<DeleteIcon/>}>Delete</Button>
                 </div>
             </div>
 
             <div className="product">
                 <h3 style={{ textAlign: 'center' }}>Purchases History</h3>
                 {storeData.purchases.filter(purchases => purchases.productId === id).map((Purchase, index) => {
-                    return (<div key={index} style={{ margin: "2rem", padding: "2rem", border: "2px solid black", borderRadius:"10px" }}>
+                    return (<div key={index} style={{ margin: "2rem", padding: "2rem", border: "2px solid black", borderRadius: "10px" }}>
                         <p><strong>Customer ID: </strong><Link to={`/editCustomer/${Purchase.customerId}`}>{Purchase.customerId}</Link></p>
                         <p><strong>Purchase ID: </strong>{Purchase.id}</p>
                         <p><strong>Purchase Date: </strong>{Purchase.date}</p>
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -2,30 +2,37 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import TotalPurchases from './TotalPurchases';
+// style scss
+import '../style/_products.scss'
 // mui
 import Typography from '@mui/material/Typography';
 import AllCustomers from './AllCustomers';
 import Container from '@mui/material/Container';
-import '../style/_products.scss'
+import Fab from '@mui/material/Fab';
+import EditIcon from '@mui/icons-material/Edit';
 
 export default function Products() {
     const storeData = useSelector((state) => state);
     return (
         <div>
             <Container>
-                <div style={{margin:"1rem", padding:"1rem", borderRadius: "15px", backgroundColor: "white", opacity: "0.8", textAlign: "center" }}>
+                <div style={{ margin: "1rem", padding: "1rem", borderRadius: "15px", backgroundColor: "white", opacity: "0.8", textAlign: "center" }}>
                     <TotalPurchases />
                     {/* <h2>All Products</h2> */}
-                    <Typography style={{ textAlign: 'left'}} variant="h3" >All Products</Typography>
+                    <Typography style={{ textAlign: 'left' }} variant="h3" >All Products</Typography>
                 </div>
 
                 {storeData.products.map((item, index) => {
                     let ProductId = item.id
                     return (<div key={index} className="product">
-                        <Typography style={{ textAlign: 'left' }} variant="h4" gutterBottom>Product ID: {item.id}</Typography>
+
+                        <div style={{display:"flex", justifyContent:"space-between"}}>
+                            <Typography style={{ textAlign: 'left' }} variant="h4" gutterBottom>Product ID: {item.id}</Typography>
+                            <Link to={`/editProduct/${item.id}`}><Fab color="info" aria-label="edit"><EditIcon/></Fab></Link>                        
+                        </div>
                         <div style={{ display: 'flex', justifyContent: "space-between", padding: "1rem" }}>
                             <div>
-                                <p><strong>Product Name: </strong><Link to={`/editProduct/${item.id}`}>{item.name}</Link></p>
+                                <p><strong>Product Name: </strong>{item.name}</p>
                                 <p><strong>Product Price: </strong>{item.price}</p>
                                 <p><strong>Quantity: </strong>{item.quantity}</p>
                             </div>
@@ -34,8 +41,8 @@ export default function Products() {
                             </div>
                         </div>
                         <div>
-                        <Typography style={{ textAlign: 'left' }} variant="h3" >Purchases</Typography>
-                        <br />
+                            <Typography style={{ textAlign: 'left' }} variant="h3" >Purchases</Typography>
+                            <br />
                             <AllCustomers id={ProductId} />
                             <br />
                         </div>
